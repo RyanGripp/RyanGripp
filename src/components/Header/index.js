a4e0ea2da9cde050f045c7971d9013c34759e7ba
RyanGripp <RyanGripp@users.noreply.github.com>
2024-03-23 01:44:22 -0300

    Apresentacao quase pronta

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import './header.css';
 
-function Header(){
-    return(
-        <header>
-            <h2>RYAN GRIPP DA SILVA</h2>
-            <nav className='navegacao'>
-                <ul>
-                    <li><a href='#'>Home</a></li>
-                    <li>Sobre</li>
-                    <li>Projetos</li>
-                </ul>
+function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        function handleScroll() {
+            if (window.scrollY > 250) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <header className={`header${isScrolled ? ' header-scroll' : ''}`}>
+            <nav className={`nav${isScrolled ? ' nav-mov' : ''}`}>
+                <a href='#home'>Home</a>
+                <a href='#sobre'>Sobre</a>
+                <a href='#projetos'>Projetos</a>
+            </nav>
+            <nav className={`nav-redes${isScrolled ? ' nav-show' : ''}`}>
+                <a href='https://www.linkedin.com/in/ryan-gripp-da-silva-b430142b8/'><i class="fa-brands fa-linkedin-in"></i></a>
+                <a href='https://github.com/RyanGripp'><i class="fa-brands fa-github"></i></a>
             </nav>
         </header>
     );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,32 +1,37 @@
 import './home.css';
 
-function Home(){
-    return(
+function Home() {
+    return (
         <div className="main">
-            {/* <FontAwesomeIcon icon="fa-brands fa-linkedin" /> */}
             <div className='inicio'>
-                <img src='/RyanGripp/SelfImage.jpg' alt='' className='selfimage'/>
-                <h2 className='nome'>Ryan Gripp da Silva</h2>
+                <div className='selfimage'><img src='/RyanGripp/SelfImage.jpg' alt='' /></div>
+                <div className='nome'><h2>Ryan Gripp da Silva</h2></div>
                 <div className='contato'>
-                    <a target='blank' href='https://www.linkedin.com/in/ryan-gripp-da-silva-b430142b8/'>LinkedIn</a>
-                    <a href='https://github.com/RyanGripp'>GitHub</a>
+                    <a target='blank' href='https://www.linkedin.com/in/ryan-gripp-da-silva-b430142b8/' className='linkedin'>
+                        <i class="fa-brands fa-linkedin"></i>
+                        <span>Linkedin</span>
+                    </a>
+                    <a href='https://github.com/RyanGripp' className='github'>
+                        <i class="fa-brands fa-github"></i>
+                        <span>GitHub</span>
+                    </a>
                 </div>
             </div>
             <div className='projeto'>
                 <img src='/RyanGripp/filmes.png' alt='' />
                 <h3>Projeto de filmes utilizando ReactJS</h3>
-                <p> Este projeto exibe uma seleção de filmes atualmente em catálogo, 
-                    fornecendo uma variedade de informações sobre cada filme. Esses dados 
-                    foram obtidos através de uma API fornecida pelo site <a target='blank' 
-                    rel='external' href='https://api.themoviedb.org/'>https://api.themoviedb.org/</a>. 
-                    O projeto permite aos usuários acessar páginas individuais para cada filme e 
-                    adicionar aos favoritos, que são armazenados localmente utilizando o 
+                <p> Este projeto exibe uma seleção de filmes atualmente em catálogo,
+                    fornecendo uma variedade de informações sobre cada filme. Esses dados
+                    foram obtidos através de uma API fornecida pelo site <a target='blank'
+                        rel='external' href='https://api.themoviedb.org/'>https://api.themoviedb.org/</a>.
+                    O projeto permite aos usuários acessar páginas individuais para cada filme e
+                    adicionar aos favoritos, que são armazenados localmente utilizando o
                     LocalStorage do navegador.
                 </p>
                 <a className='ver-projeto' target='blank' rel='external' href='https://ryangripp-filmes.netlify.app/'>
-                Ver Projeto</a>
+                    Ver Projeto</a>
                 <a className='ver-repositorio' target='blank' rel='external' href='https://github.com/RyanGripp/Api-Filmes'>
-                Ver Repositorio</a>
+                    Ver Repositorio</a>
             </div>
         </div>
     )
